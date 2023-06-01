c92a011ebf959b7ef2d849a154624020b8b26089
“yujunxiangsizhishiqing” <yujunxiangsizhishiqing@yujunxiangsizhishiqingdeMacBook-Air.local>
2023-06-01 17:21:20 +0800

    update pom.xml and rename some file and className

--- src/main/java/com/springbootapp/ToByteDanceApplication.java
+++ src/main/java/com/springbootapp/ToByteDanceApplication.java
@@ -1,11 +1,11 @@
-package com;
+package com.springbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class toByteDanceApplication {
+public class ToByteDanceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(toByteDanceApplication.class, args);
+        SpringApplication.run(ToByteDanceApplication.class, args);
     }
 }
